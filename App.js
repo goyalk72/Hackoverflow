a76fd7976b672fd7f9fe6e2a92520080dc2d9afa
Raj S. Jagtap <46604793+jagtapraj123@users.noreply.github.com>
2021-01-16 00:16:12 +0530

    Add files via upload
    Complaint Form part of the WebApp
    Install @material-ui/core & final-form & react-final-form.

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ComplaintForm from './complaint_form/complaintForm.js'
+
+function App() {
+  return (
+    <div className="App">
+      <ComplaintForm />
+    </div>
+  );
+}
+
+export default App;
