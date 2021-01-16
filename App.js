30268596fcb5ef9d3c0b6460753b4152cf94195c
Raj S. Jagtap <46604793+jagtapraj123@users.noreply.github.com>
2021-01-16 20:15:58 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import ComplaintForm from './complaint_form/complaintForm.js'
+import ComplaintForm from './ComplaintForm.js'
+import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+import Login from './components/Login/Login';
+import AuthLogin from './components/AuthLogin/AuthLogin';
 
-function App() {
-  return (
-    <div className="App">
-      <ComplaintForm />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/admin" component={AuthLogin}></Route>
+        <Route path="/" render={() => (<Redirect to="/login" />)} />
+      </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
